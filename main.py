41bfdb556eed01e1fb741090fe2ec1ec0d17bdb7
RJW <blacksheeprobotics@gmail.com>
2024-02-18 22:21:03 -0500

    added turtle player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import turtle
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
@@ -8,7 +9,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.move_forward, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
